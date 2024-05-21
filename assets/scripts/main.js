a9c28e5fae0d7f634a47424ec757e40ce4a2cacf
P7chang <p7chang@ucsd.edu>
2024-05-21 03:51:59 -0700

    expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,15 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+
+	const rcp = localStorage.getItem('recipes');
+	if (rcp === null) {
+		//console.log('NULL!');
+		return [];
+	} else {
+		return JSON.parse(rcp);
+	}
+
 }
 
 /**
@@ -35,10 +44,19 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+
+	let mainRef = document.querySelector('main');
+
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+
+	for (let i = 0; i < recipes.length; i++) {
+		let curEl = document.createElement('recipe-card');
+		curEl.data = recipes[i];
+		mainRef.append(curEl);
+	}
 }
 
 /**
